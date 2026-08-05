534a05ed9f614d91c5be688a90e4af27bf731995
josecont8920-boop <tu_correo@ejemplo.com>
2026-08-05 10:53:27 -0400

    fix: limpia estructura raiz para dejar proyecto original

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.usuario import Usuario, RolUsuario  # noqa
-from app.models.cliente import Cliente  # noqa
-from app.models.factura import Factura, FacturaItem, Cuota, EstadoFactura, EstadoMora  # noqa
-from app.models.pago import Pago, Recibo, MetodoPago, TipoPago  # noqa
